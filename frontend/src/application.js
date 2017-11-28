73e31e678c817808b0c2a986adc15493767e695c
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-11-28 23:57:01 +0200

    front init

---
+++ frontend/src/application.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './application.css';
+
+class Application extends Component {
+    render() {
+        return (
+            <div className="application">...</div>
+        );
+    }
+}
+
+export default Application;
