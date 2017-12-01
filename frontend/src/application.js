6f32fea1f7a6ceb7bb05444e9edb5f110a5e5e5a
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-12-01 22:11:45 +0200

    app markup

--- frontend/src/application.js
+++ frontend/src/application.js
@@ -4,7 +4,19 @@ import './application.css';
 class Application extends Component {
     render() {
         return (
-            <div className="application">...</div>
+            <div className="application">
+                <div>Request type: translation or improvement</div>
+                <div>
+                    Language: (try to figure out before it is shown)
+                </div>
+                <div>
+                    Request:
+                    <input type="text"/>
+                </div>
+                <div>
+                    <button>Submit</button>
+                </div>
+            </div>
         );
     }
 }
