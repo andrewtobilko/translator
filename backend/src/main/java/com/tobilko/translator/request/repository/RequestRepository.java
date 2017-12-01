dd923d270ba527a8e4bd05a5934d7b28e3ed2a4a
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-12-01 22:26:19 +0200

    findAllByAuthorId added

--- backend/src/main/java/com/tobilko/translator/request/repository/RequestRepository.java
+++ backend/src/main/java/com/tobilko/translator/request/repository/RequestRepository.java
@@ -3,9 +3,13 @@ package com.tobilko.translator.request.repository;
 import com.tobilko.translator.request.Request;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by Andrew Tobilko on 12/1/17.
  */
 public interface RequestRepository extends CrudRepository<Request, Long> {
+
+    List<Request> findAllByAuthorId(Long authorId);
 
 }
--- backend/src/main/java/com/tobilko/translator/response/repository/ResponseRepository.java
+++ backend/src/main/java/com/tobilko/translator/response/repository/ResponseRepository.java
@@ -3,7 +3,13 @@ package com.tobilko.translator.response.repository;
 import com.tobilko.translator.response.Response;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by Andrew Tobilko on 12/1/17.
  */
-public interface ResponseRepository extends CrudRepository<Response, Long> {}
+public interface ResponseRepository extends CrudRepository<Response, Long> {
+
+    List<Response> findAllByAuthorId(Long authorId);
+
+}
