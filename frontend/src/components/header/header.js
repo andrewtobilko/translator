d827bd1e04ed041303744530e4119cfc3739320b
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-12-03 19:07:11 +0200

    header

---
+++ frontend/src/components/header/header.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './header.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <div>LOGO</div>
+                <nav>
+                    <a href="/home">Home</a>
+                    <a href="/request/translate">Translate</a>
+                    <a href="/request/improve">Improve</a>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Header;
