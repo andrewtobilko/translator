2a55968f8d800207bd8d5ae080635eaf8b1008cd
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-12-03 19:06:53 +0200

    footer

---
+++ frontend/src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './footer.css'
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                All rights reserved.
+            </footer>
+        );
+    }
+}
+
+export default Footer;
