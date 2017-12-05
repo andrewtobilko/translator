b5fd2979484decbfef6e03c4a7f6284535e665a3
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-12-06 01:39:27 +0200

    testReducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,7 +2,19 @@ import {combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form'
 
 const rootReducer = combineReducers({
-    form: formReducer
+    form: formReducer,
+    testReducer: testReducer
 });
+
+function testReducer(data) {
+    if (!data) {
+        return {
+            state: "initialState"
+        };
+    }
+    console.log('my reducer data ', data);
+
+    return data;
+}
 
 export default rootReducer;
