423ce365a26a1e36190197859577a686ad50b6d7
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-12-06 01:37:02 +0200

    createStoreWithMiddleware

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import {Provider} from 'react-redux'
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from "react-router-dom";
 import Application from "./components/application";
+import ReduxPromise from 'redux-promise'
+import {applyMiddleware, createStore} from "redux";
+import reducers from './reducers';
 
-ReactDOM.render((
-    <BrowserRouter>
-        <Application/>
-    </BrowserRouter>
-), document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <Application/>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,4 +1,4 @@
-import {combineReducers, createStore} from 'redux';
+import {combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form'
 
 const rootReducer = combineReducers({
